57a46d74805c6eddc5ccfa41c9ebc85f3165460d
Jack <jim3470022@gmail.com>
2025-02-24 14:30:40 +0800

    feat:限制頁面出現卡片數量/加入分頁功能/導入搜尋框UI

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Searchbar from "../components/Searchbar";
+import Banner from "../components/Banner";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
@@ -49,11 +51,27 @@ export default function Results() {
     );
   };
 
+  //限制卡片出現5筆資料
+
+  const [currentPage, setCurrentPage] = useState(1); //當前頁數
+  const itemsPerPage = 5; //每頁顯示5筆資料
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const displayProducts = products.slice(startIndex, endIndex);
+
+  //切換分頁
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    //滾動到頁面頂部
+    window.scrollTo({ top: 500, behavior: "smooth" });
+  };
+
   return (
     <>
       <div className="main">
         {/* banner部分 */}
-        <section className="index-banner">
+        {/* <section className="index-banner">
           <div className="card border-0 rounded-0">
             <div
               className="banner-image d-md-inline-block d-none "
@@ -82,26 +100,25 @@ export default function Results() {
               </p>
             </div>
           </div>
-        </section>
+        </section> */}
+        <Banner />
 
         {/* 搜尋結果卡片 */}
         <div className="content pt-12 mt-md-14 result-content">
           <div className="container">
             <div className="row d-flex flex-column gy-7 gy-lg-9">
-              {products.map((product) => (
+              {displayProducts.map((product) => (
                 <div className="col" key={product.id}>
                   <div className="cards shadow rounded">
                     <div className="card mb-3">
                       <div className="row g-0">
-                        <div className="col-md-5 ">
-
-                            <img
-                              className="card-img img-fluid result-card"
-                              src={product?.thumbs?.[0]}
-                              alt="building"
-                            />
-                            <HeartCard />
-                          
+                        <div className="col-md-5 " style={{ height: "290px" }}>
+                          <img
+                            className="card-img img-fluid result-card"
+                            src={product?.thumbs?.[0]}
+                            alt="building"
+                          />
+                          <HeartCard />
                         </div>
                         <div className="col-md-7">
                           <div className="card-body">
@@ -200,38 +217,61 @@ export default function Results() {
                 </div>
               ))}
             </div>
-
-            <div className="pagnation d-lg-flex py-8 justify-content-center d-none">
-              <div className="left d-flex align-items-center">
-                <a className="d-flex align-items-center" href="#">
-                  <span className="material-symbols-outlined text-secondary-80 ">
-                    <img
-                      src="./src/assets/images/Icon/IconSecondary/VectorLeft-S-80.svg"
-                      alt="left"
-                    />
-                  </span>
-                </a>
-              </div>
-
-              <div className="pagnation-num px-6">
-                <p className="text-secondary-50">1/20</p>
-              </div>
-
-              <div className="right d-flex align-items-center">
-                <a className="d-flex align-items-center" href="#">
-                  <span className="material-symbols-outlined text-secondary-80">
-                    <img
-                      src="./src/assets/images/Icon/IconSecondary/VectorRight-S-80.svg"
-                      alt="left"
-                    />
-                  </span>
-                </a>
-              </div>
-            </div>
           </div>
         </div>
+
+        {/* 分頁 */}
+        <nav aria-label="...">
+          {/* 上一頁 */}
+          <ul className="pagination d-lg-flex py-8 justify-content-center d-none">
+            <li className={`page-item ${currentPage === 1 ? "disabled" : ""}`}>
+              <button
+                className={`page-link ${currentPage === 1 && "disabled"} `}
+                style={{ background: "transparent", border: "none" }}
+                onClick={() => handlePageChange(currentPage - 1)}
+              >
+                <img
+                  src="./src/assets/images/Icon/IconSecondary/VectorLeft-S-80.svg"
+                  alt="left"
+                />
+              </button>
+            </li>
+            {/* 頁數 */}
+
+            {Array.from({ length: itemsPerPage }).map((_, index) => (
+              <li
+                key={index}
+                className={`page-item ${
+                  currentPage === index + 1 ? "active " : ""
+                }`}
+              >
+                <button
+                  className="page-link text-secondary-50"
+                  style={{ background: "transparent", border: "none" }}
+                  onClick={() => handlePageChange(index + 1)}
+                >
+                  {index + 1}
+                </button>
+              </li>
+            ))}
+
+            <li className={`page-item `}>
+              <button
+                className={`page-link ${
+                  currentPage === products.length / itemsPerPage && "disabled"
+                }`}
+                style={{ background: "transparent", border: "none" }}
+                onClick={() => handlePageChange(currentPage + 1)}
+              >
+                <img
+                  src="./src/assets/images/Icon/IconSecondary/VectorRight-S-80.svg"
+                  alt="left"
+                />
+              </button>
+            </li>
+          </ul>
+        </nav>
       </div>
     </>
   );
 }
-

00c204b1214c24f220e2117e5cbbe9c19f950fc2
Jack <jim3470022@gmail.com>
2025-03-05 15:39:48 +0800

    fix:預定留床按鍵元件匯入

--- src/pages/FrontLayout.jsx
+++ src/pages/FrontLayout.jsx
@@ -11,13 +11,23 @@ export default function FrontLayout() {
   const [isLogin, setIsLogin] = useState(false);
   const [userName, setUserName] = useState("");
   const loginModalRef = useRef(null);
-  const [ isLoginModalOpen, setIsLoginModalOpen ] = useState(false);
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
   const [loginModalMode, setLoginModalMode] = useState("");
   return (
     <>
       {/* 讓 isLogin, setIsLogin, userName, setUserName 可以在前台網頁的其他元件中用 useContext 引入使用 */}
       <UserContext.Provider
-        value={{ isLogin, setIsLogin, userName, setUserName,loginModalRef,isLoginModalOpen,setIsLoginModalOpen,loginModalMode,setLoginModalMode }}
+        value={{
+          isLogin,
+          setIsLogin,
+          userName,
+          setUserName,
+          loginModalRef,
+          isLoginModalOpen,
+          setIsLoginModalOpen,
+          loginModalMode,
+          setLoginModalMode,
+        }}
       >
         <ScrollToTop />
         <Header></Header>

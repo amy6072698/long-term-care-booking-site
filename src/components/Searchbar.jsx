0d558563504e7b1ae847d3b5a0adcd8e996e970c
Jack <jim3470022@gmail.com>
2025-03-12 22:04:45 +0800

    fix:修改縣市資料庫

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -4,16 +4,11 @@ import { useForm } from "react-hook-form";
 import { useNavigate, useSearchParams } from "react-router";
 
 // 引入 ResultsLayout 中的 SearchContext
-import { SearchContext } from '../pages/ResultsLayout';
+import { SearchContext } from "../pages/ResultsLayout";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-
-// 建立 機構類型 照護需求 醫療需求 的搜尋框下拉選項 
-const categories = [
-  "長期照顧中心",
-  "護理之家",
-  "安養中心",
-  "日間照顧中心"
-]
+// 建立 機構類型 照護需求 醫療需求 的搜尋框下拉選項
+const categories = ["長期照顧中心", "護理之家", "安養中心", "日間照顧中心"];
 // tag：services.name
 // care: caringItem
 const caringItems = [
@@ -27,7 +22,7 @@ const caringItems = [
   // "認知障礙（失智）",X
   // tag:5失智照顧
 
-  "復健照護"
+  "復健照護",
   // 自己
   // tag:6免外出復健
 ];
@@ -44,7 +39,7 @@ const medicalServices = [
   // "藥師評估",X
   // "營養評估",
   // tag:7個別化飲食
-  
+
   "腫瘤照護",
   // "腫瘤照顧",
   // care："腫瘤照護（癌症）",
@@ -52,7 +47,7 @@ const medicalServices = [
   "洗腎照護",
   // "洗腎接送",
   // care："透析照護（洗腎）"
-  
+
   "術後傷口",
   // "術後照顧"
   // "傷口照顧"X
@@ -63,73 +58,68 @@ const medicalServices = [
   // tag:3特殊沐浴設備
   // "如廁訓練",X
   // "延緩失能"X
-]
+];
 
-function Searchbar(){
+function Searchbar() {
+  const [cityData, setCityData] = useState([]);
+  const [regionData, setRegionData] = useState([]);
 
-  const [ cityData, setCityData ] = useState([]);
-  const [ regionData, setRegionData ] = useState([]);
+  const [cityValue, setCityValue] = useState("");
+  const [regionValue, setRegionValue] = useState("");
 
-  const [ cityValue, setCityValue ] = useState("");
-  const [ regionValue, setRegionValue ] = useState("");
-
-  // 取得 縣市 地區 搜尋框下拉選項 
-  const getTaiwanCity = async() => {
-    const res = await axios.get('/taiwancity.json');
+  // 取得 縣市 地區 搜尋框下拉選項
+  const getTaiwanCity = async () => {
+    const res = await axios.get(`${BASE_URL}/taiwancitys`);
     setCityData(res.data);
-  }
+  };
 
   useEffect(() => {
     getTaiwanCity();
     // console.log(cityData);
-  },[])
+  }, []);
 
   // 當縣市改變取得相應的地區選項資料更新至 regionData ，並用 cityValue 讓電腦版與手機板縣市選取的值同步
   const handleCityChange = (e) => {
-    if (e.target.value !== '') {
-      // 更新選取的值到 cityValue 
-      setCityValue(e.target.value)
+    if (e.target.value !== "") {
+      // 更新選取的值到 cityValue
+      setCityValue(e.target.value);
 
       // 找到符合的縣市物件
-      const selectedCity = cityData.find((item) => (item.name === e.target.value));
+      const selectedCity = cityData.find(
+        (item) => item.name === e.target.value
+      );
 
       // 將符合的縣市內的 regions 陣列更新至 regionData
       setRegionData([...selectedCity.regions]);
-
     } else {
-      setCityValue('');
+      setCityValue("");
       setRegionData([]);
     }
-  }
+  };
 
   // 用 regionValue 讓電腦版與手機板地區選取的值同步
   const handleRegionChange = (e) => {
-    if (e.target.value !== '') {
-      // 更新選取的值到 regionValue 
-      setRegionValue(e.target.value)
+    if (e.target.value !== "") {
+      // 更新選取的值到 regionValue
+      setRegionValue(e.target.value);
     } else {
-      setRegionValue('');
+      setRegionValue("");
     }
-  }
+  };
 
   // react hook form 引入及預設值設定
-  const {
-    register,
-    handleSubmit,
-  } = useForm(
-    {
-      // 表單預設值
-      defaultValues: {
-        category: "",
-        city: "",
-        city_mobile:"",
-        region: "",
-        region_mobile:"",
-        caringItem: "",
-        medicalService: ""
-      }
-    }
-  )
+  const { register, handleSubmit } = useForm({
+    // 表單預設值
+    defaultValues: {
+      category: "",
+      city: "",
+      city_mobile: "",
+      region: "",
+      region_mobile: "",
+      caringItem: "",
+      medicalService: "",
+    },
+  });
 
   // 用於更新網址搜尋參數
   const { setSearchParams } = useContext(SearchContext);
@@ -142,12 +132,10 @@ function Searchbar(){
 
   // 選好搜尋項目、提交搜尋內容後執行
   const handleSearchSubmit = handleSubmit((data) => {
-
     // 解構 data 中 需要用到的 值，提交後 data 有各項下拉框內的提交值
     const { category, city, region, caringItem, medicalService } = data;
     // console.log(data);
 
-    
     // 將 機構類型 提交值加入 params
     if (category) {
       params.append("category_like", category);
@@ -164,16 +152,14 @@ function Searchbar(){
     // 判斷 照護需求 提交值加入相應的內容到 params
     if (
       caringItem === "鼻胃管" ||
-      caringItem === "導尿管" || 
-      caringItem === "氣切管" || 
+      caringItem === "導尿管" ||
+      caringItem === "氣切管" ||
       caringItem === "氧氣"
     ) {
       params.append("caringItem_like", caringItem);
-
     } else if (caringItem === "精神照護") {
       params.append("caringItem_like", caringItem);
       params.append("services.id", 5);
-
     } else if (caringItem === "復健照護") {
       params.append("caringItem_like", caringItem);
       params.append("services.id", 6);
@@ -183,130 +169,157 @@ function Searchbar(){
     if (medicalService === "巡診接送") {
       params.append("medicalService_like", "醫師巡診");
       params.append("services.id", 2);
-
     } else if (medicalService === "鄰近就醫") {
       params.append("medicalService_like", "鄰近醫院");
       params.append("services.id", 1);
-
     } else if (medicalService === "特殊飲食") {
       params.append("medicalService_like", "營養評估");
       params.append("services.id", 7);
-
     } else if (medicalService === "腫瘤照護") {
       params.append("medicalService_like", "腫瘤照顧");
       params.append("caringItem_like", "腫瘤照護（癌症）");
-
-    } else if (medicalService === "洗腎照護"){
+    } else if (medicalService === "洗腎照護") {
       params.append("medicalService_like", "洗腎接送");
       params.append("caringItem_like", "透析照護（洗腎）");
-
-    } else if (medicalService === "術後傷口"){
+    } else if (medicalService === "術後傷口") {
       params.append("medicalService_like", "術後照顧");
       params.append("services.id", 4);
-      
-    } else if (medicalService === "生活照顧"){
+    } else if (medicalService === "生活照顧") {
       params.append("medicalService_like", medicalService);
       params.append("services.id", 3);
-
     }
 
     // 將 params 的內容更新到 searchParams
     setSearchParams(params);
 
     // 判斷 params 是否為空，是就不加入網址參數，否就加入網址參數
-    if(params){
+    if (params) {
       navigate(`results/search?${params.toString()}`);
     } else {
       navigate(`results/search`);
     }
-
-  })
-  
-
+  });
 
   return (
-
     <div className="searchbar container-lg container-fluid">
-      <form onSubmit={handleSearchSubmit} className="search-wrapper d-flex flex-md-row flex-column" action="">
-        <select 
-        {...register("category")}
-        className="form-select fs-lg-6 fs-md-7 fs-6" aria-label="Default select example">
+      <form
+        onSubmit={handleSearchSubmit}
+        className="search-wrapper d-flex flex-md-row flex-column"
+        action=""
+      >
+        <select
+          {...register("category")}
+          className="form-select fs-lg-6 fs-md-7 fs-6"
+          aria-label="Default select example"
+        >
           <option value="">機構類型</option>
           {categories.map((item, index) => (
-            <option key={index} value={item}>{item}</option>
+            <option key={index} value={item}>
+              {item}
+            </option>
           ))}
         </select>
 
-        <select 
-        {...register("city")} 
-        onChange={handleCityChange} 
-        value={cityValue} 
-        className="form-select fs-lg-6 fs-md-7 fs-6 d-md-block d-none" aria-label="Default select example">
+        <select
+          {...register("city")}
+          onChange={handleCityChange}
+          value={cityValue}
+          className="form-select fs-lg-6 fs-md-7 fs-6 d-md-block d-none"
+          aria-label="Default select example"
+        >
           <option value="">縣市</option>
           {cityData.map((item, index) => (
-            <option key={index} value={item.name}>{item.name}</option>
+            <option key={index} value={item.name}>
+              {item.name}
+            </option>
           ))}
         </select>
-        <select 
-        {...register("region")} 
-        onChange={handleRegionChange} 
-          value={regionValue} 
-        className="form-select fs-lg-6 fs-md-7 fs-6 d-md-block d-none" aria-label="Default select example">
+        <select
+          {...register("region")}
+          onChange={handleRegionChange}
+          value={regionValue}
+          className="form-select fs-lg-6 fs-md-7 fs-6 d-md-block d-none"
+          aria-label="Default select example"
+        >
           <option value="">地區</option>
           {regionData.map((item, index) => (
-            <option key={index} value={item}>{item}</option>
+            <option key={index} value={item}>
+              {item}
+            </option>
           ))}
         </select>
 
         {/* 手機版 */}
         <div className="search-wrapper-sm d-flex d-md-none">
-          <select 
-          {...register("city_mobile")} 
-          onChange={handleCityChange} 
-          value={cityValue} 
-          className="form-select fs-6 w-50" aria-label="Default select example">
+          <select
+            {...register("city_mobile")}
+            onChange={handleCityChange}
+            value={cityValue}
+            className="form-select fs-6 w-50"
+            aria-label="Default select example"
+          >
             <option value="">縣市</option>
             {cityData.map((item, index) => (
-              <option key={index} value={item.name}>{item.name}</option>
+              <option key={index} value={item.name}>
+                {item.name}
+              </option>
             ))}
           </select>
-          <select 
-          {...register("region_mobile")} 
-          onChange={handleRegionChange} 
-          value={regionValue} 
-          className="form-select fs-6 w-50" aria-label="Default select example">
+          <select
+            {...register("region_mobile")}
+            onChange={handleRegionChange}
+            value={regionValue}
+            className="form-select fs-6 w-50"
+            aria-label="Default select example"
+          >
             <option value="">地區</option>
             {regionData.map((item, index) => (
-              <option key={index} value={item}>{item}</option>
+              <option key={index} value={item}>
+                {item}
+              </option>
             ))}
           </select>
         </div>
 
-        <select 
-        {...register("caringItem")}
-        className="form-select fs-lg-6 fs-md-7 fs-6" aria-label="Default select example">
+        <select
+          {...register("caringItem")}
+          className="form-select fs-lg-6 fs-md-7 fs-6"
+          aria-label="Default select example"
+        >
           <option value="">照護需求</option>
           {caringItems.map((item, index) => (
-            <option key={index} value={item}>{item}</option>
+            <option key={index} value={item}>
+              {item}
+            </option>
           ))}
         </select>
-        <select 
-        {...register("medicalService")}
-        className="form-select fs-lg-6 fs-md-7 fs-6" aria-label="Default select example">
+        <select
+          {...register("medicalService")}
+          className="form-select fs-lg-6 fs-md-7 fs-6"
+          aria-label="Default select example"
+        >
           <option value="">醫療需求</option>
           {medicalServices.map((item, index) => (
-            <option key={index} value={item}>{item}</option>
+            <option key={index} value={item}>
+              {item}
+            </option>
           ))}
         </select>
         <div className="d-lg-block d-grid">
-          <button type="submit" className="search-primary-btn btn-width btn btn-primary-40 rounded-1 py-5 px-0 fs-lg-5 fs-md-6 fs-5 text-black d-flex justify-content-center align-items-center gap-lg-4 gap-md-0 gap-4">
-            <img src="https://raw.githubusercontent.com/Jack-Xiao-2024/Project_D01/972c057941c61b13fd6db44e084aac94405b25ed/assets/images/Icon/IconBlack/Search-B.svg" alt="search icon"/>
+          <button
+            type="submit"
+            className="search-primary-btn btn-width btn btn-primary-40 rounded-1 py-5 px-0 fs-lg-5 fs-md-6 fs-5 text-black d-flex justify-content-center align-items-center gap-lg-4 gap-md-0 gap-4"
+          >
+            <img
+              src="https://raw.githubusercontent.com/Jack-Xiao-2024/Project_D01/972c057941c61b13fd6db44e084aac94405b25ed/assets/images/Icon/IconBlack/Search-B.svg"
+              alt="search icon"
+            />
             <h5 className="fs-lg-5 fs-md-6">搜尋合適機構</h5>
           </button>
         </div>
       </form>
     </div>
-  )
+  );
 }
 
 export default Searchbar;
